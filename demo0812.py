b6fb0e911f60dca02fab276f17db66a8cd810afd
wangxiaoxiao <2458598677@qq.com>
2021-08-16 23:16:58 +0800

    demo0812提交代码20210816

--- demo0812.py
+++ demo0812.py
@@ -33,20 +33,63 @@
 #class 声明类的名称 类的名字首字母必须大写
 #class Test():  类里面所有的方法都必须传一个参数
 #面向对象编程 （类=对象）
-class GirFriend():
-    def _init_(self):    #类的属性
-        self.sex="女"
-        self.high="170cm"
-        self.weight="55kg"    
-        self.hair="大波浪" 
-        self.age="18岁"
-    def caiyi(self,num):  #才艺表演
-        if num==1:
-            print("胸口碎大石！")
-        elif num==2:
-            print("唱跳")
-        else:
-            print("单手开瓶盖")
+# class GirlFriend():
+#     # def __init__(self):    #类的属性
+#     #     self.sex="女"
+#     #     self.high="170cm"
+#     #     self.weight="55kg"    
+#     #     self.hair="大波浪" 
+#     #     self.age="18岁"
+    
+#     def __init__(self,sex,high,weight,hair,age):
+#         self.sex=sex
+#         self.high=high
+#         self.weight=weight
+#         self.hair=hair
+#         self.age=age
 
-zhangsan=GirFriend()
-zhangsan.caiyi(2)
+#     def caiyi(self,num):  #才艺表演
+#         print("性别为",self.sex,"开始才艺表演")
+#         if num==1:
+#             print("胸口碎大石！")
+#         elif num==2:
+#             print("唱跳")
+#         else:
+#             print("单手开瓶盖")
+#     def chuyi(self):  #厨艺
+#         print("体重为"+self.weight+"开始厨艺表演")
+#         print("精通八大菜系")
+#     def work(self):   #工作
+#         print("身高为"+self.high+"开始工作")
+#         print("开挖掘机")
+
+# #类的实例化
+# # zhangsan=GirlFriend()
+# zhangsan=GirlFriend("女","170cm","45kg","直发","18")
+# zhangsan.caiyi(1)
+# zhangsan.chuyi()
+# zhangsan.work()
+# print(zhangsan.high)
+
+class Car():
+    def __init__(self,appearance,brand):
+        self.appearance=appearance
+        self.brand=brand
+    def fly(self):
+        print(self.appearance+"的"+self.brand)
+        print("会飞")
+    def transform(self):
+        print("变形")
+landRover=Car("高端大气","路虎")
+landRover.fly()
+landRover.transform()
+
+class NewCar(Car):  #newcar继承新车
+    def fly(self):
+        print(self.appearance+"的"+self.brand)
+        print("不会飞哦")
+Tesla=NewCar("小巧","特斯拉")
+Tesla.fly()
+        
+
+
